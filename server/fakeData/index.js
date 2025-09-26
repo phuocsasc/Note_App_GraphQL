d32185094c83913521b0e60d0ecdcffffed3cd6b
phuocsasc <phuoctran.22102004@gmail.com>
2025-09-26 12:01:08 +0700

    chore: initial commit (client + server + config)

---
+++ server/fakeData/index.js
@@ -0,0 +1,32 @@
+export default {
+    authors: [
+        {
+            id: 123,
+            name: 'Phuoc',
+        },
+        {
+            id: 999,
+            name: 'Nam',
+        },
+    ],
+    folders: [
+        {
+            id: 1,
+            name: 'Home',
+            createdAt: '2025-10-18T03:42:132Z',
+            authorId: 123,
+        },
+        {
+            id: 2,
+            name: 'New Folder',
+            createdAt: '2025-11-18T03:42:132Z',
+            authorId: 999,
+        },
+        {
+            id: 3,
+            name: 'Word',
+            createdAt: '2025-09-18T03:42:132Z',
+            authorId: 123,
+        },
+    ],
+};
