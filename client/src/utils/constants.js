713ad37efd01f8311cd659b2ca5551a1f5715881
phuocsasc <phuoctran.22102004@gmail.com>
2025-10-03 10:23:37 +0700

    update client url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER_URL = "https://note-app-graphql-server-enof.onrender.com";
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "ws://note-app-graphql-server-enof.onrender.com/graphql";
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = "wss://note-app-graphql-server-enof.onrender.com/graphql";
